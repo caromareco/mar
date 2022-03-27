abc249b62ee0d8471d0d67f9cf277452261353f2
caromareco <caro.mareco94@gmail.com>
2022-03-27 17:10:00 -0300

    se arregla

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
 	const imgLightBox = document.querySelectorAll('.materialboxed');
 	M.Materialbox.init(imgLightBox, {
@@ -5,3 +32,4 @@ document.addEventListener('DOMContentLoaded', () => {
 		outDuration: 500
 	});
 });
+
